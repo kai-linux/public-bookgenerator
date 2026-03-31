2b14c16ae66bf830ac3c01276072cdc225660343
kai-linux <kai.libicher@gmail.com>
2026-03-31 11:39:35 +0200

    Update dependencies and fix lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    settings: {
+      next: {
+        rootDir: "bookgenerator-frontend/",
+      },
+    },
+  },
+];
 
 export default eslintConfig;
